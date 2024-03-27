1457c0cc7a4b23e29de0d321fa7d4dbba3d10870
Nikita-Vaid <vaidnikita06@gmail.com>
2024-03-27 17:46:42 +0530

    first

---
+++ backend/main.py
@@ -0,0 +1,100 @@
+# uvicorn main:app
+# uvicorn main:app --reload
+# source venv/Scripts/activate
+
+# Main Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from decouple import config
+import openai
+
+# Custom Function Imports
+from functions.database1 import store_messages, reset_messages
+from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.text_to_speech import convert_text_to_speech
+
+
+# Initiate App
+app = FastAPI()
+
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Check health
+@app.get("/health")
+async def check_health():
+    return {"response": "healthy"}
+
+# Reset conversation
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()  
+    return {"message": "conversation reset"}
+
+
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+
+    # Convert audio to text - production
+    # Save the file temporarily
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+
+
+    # Decode Audio
+    message_decoded = convert_audio_to_text(audio_input)  
+
+    # Guard: Ensure output
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+    
+    # Get chat response
+    chat_response = get_chat_response(message_decoded)
+
+    # Store messages
+    # store_messages(message_decoded, chat_response)
+
+
+     # Guard: CHATGPT response 
+    if not chat_response:
+        raise HTTPException(status_code=400, detail="Failed to get chat response")
+    
+
+    # Store messages 
+    store_messages(message_decoded, chat_response)
+
+    # Convert chat response to audio
+    print(chat_response)
+    audio_output = convert_text_to_speech(chat_response)
+
+    # Guard: CHATGPT response 
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed to get audio response")
+    
+    
+    # Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+
+   # Use for Post: Return output audio
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
+
+    
---
+++ frontend/src/components/Controller.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import Title from "./Title";
+import axios from "axios";
+import RecordMessage from "./RecordMessage";
+
+const Controller = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<any[]>([]);
+
+  function createBlobURL(data: any) {
+    const blob = new Blob([data], { type: "audio/mpeg" });
+    const url = window.URL.createObjectURL(blob);
+    return url;
+  }
+
+  const handleStop = async (blobUrl?: string) => {
+    setIsLoading(true);
+
+    try {
+      if (!blobUrl) return;
+
+      // Fetch recorded audio blob
+      const response = await fetch(blobUrl);
+      const blob = await response.blob();
+
+      // Construct form data with audio blob
+      const formData = new FormData();
+      formData.append("file", blob, "myFile.wav");
+
+      // Send form data to API endpoint
+      const res = await axios.post("http://localhost:8000/post-audio", formData, {
+        headers: {
+          "Content-Type": "audio/mpeg",
+        },
+        responseType: "arraybuffer", // Set the response type to handle binary data
+      });
+
+      // Create blob URL from response data
+      const audioBlobUrl = createBlobURL(res.data);
+
+      // Append Rachel's message to messages
+      const rachelMessage = { sender: "rachel", blobUrl: audioBlobUrl };
+      setMessages((prevMessages) => [...prevMessages, rachelMessage]);
+
+      // Play audio
+      const audio = new Audio(audioBlobUrl);
+      audio.play();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="h-screen overflow-y-hidden">
+      {/* Title */}
+      <Title setMessages={setMessages} />
+
+      <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        {/* Conversation */}
+        <div className="mt-5 px-5">
+          {messages?.map((audio, index) => (
+            <div
+              key={index + audio.sender}
+              className={"flex flex-col " + (audio.sender === "rachel" && "flex items-end")}
+            >
+              {/* Sender */}
+              <div className="mt-4 ">
+                <p className={audio.sender === "rachel" ? "text-right mr-2 italic text-green-500" : "ml-2 italic text-blue-500"}>
+                  {audio.sender}
+                </p>
+
+                {/* Message */}
+                <audio src={audio.blobUrl} className="appearance-none" controls />
+              </div>
+            </div>
+          ))}
+
+          {messages.length === 0 && !isLoading && (
+            <div className="text-center font-light italic mt-10">
+              Send Rachel a message...
+            </div>
+          )}
+
+          {isLoading && (
+            <div className="text-center font-light italic mt-10 animate-pulse">
+              Gimme a few seconds...
+            </div>
+          )}
+        </div>
+
+        {/* Recorder */}
+        <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
+          <div className="flex justify-center items-center w-full">
+            <div>
+              <RecordMessage handleStop={() => handleStop()} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Controller;
